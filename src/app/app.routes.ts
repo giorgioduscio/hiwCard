81ebca9ccaac727bcf8c7e9c332ddee33164f36e
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-06-11 11:44:15 +0200

    router children

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { UsersComponent } from './components/users/users.component';
+import { BadgeComponent } from './components/users/badge/badge.component';
 
 export const routes: Routes =[
     {path: "", redirectTo: "users", pathMatch: 'full'},
-    {path: "users", component: UsersComponent},
     {path: "card/:ID", component: CardComponent},
+    {path: "users", component: UsersComponent, children:[
+        {path:":id", component: BadgeComponent},
+    ]},
 ];
 
---
+++ src/app/components/users/badge/badge.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, RouterLink } from '@angular/router';
+import { FieldsService } from '../../../services/fields.service';
+
+@Component({
+  selector: 'app-badge',
+  standalone: true,
+  imports: [
+    RouterLink,
+  ],
+  templateUrl: './badge.component.html',
+  styleUrl: './badge.component.css'
+})
+export class BadgeComponent implements OnInit{
+  id!: number;
+  character:any
+
+  constructor(private route: ActivatedRoute, private field: FieldsService){}
+
+  ngOnInit(): void {
+    // PRENDE L'ID DALLA BARRA DEL BROWSER  
+
+    this.route.paramMap.subscribe((p: ParamMap)=>{
+      this.id = +p.get('id')!
+      this.character =this.field.getCharacters()[this.id]
+      
+    })
+  }
+}
