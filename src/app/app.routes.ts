970f86bc99906f97b31d46818b193b23fd3617c4
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-06-12 10:26:43 +0200

    not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,16 @@ import { Routes } from '@angular/router';
 import { CardComponent } from './components/card/card.component';
 import { UsersComponent } from './components/users/users.component';
 import { BadgeComponent } from './components/users/badge/badge.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 export const routes: Routes =[
     {path: "", redirectTo: "users", pathMatch: 'full'},
     {path: "card/:ID", component: CardComponent},
     {path: "users", component: UsersComponent, children:[
         {path:":id", component: BadgeComponent},
-    ]},
+        ]},
+        
+    {path: "404", component: NotFoundComponent},
+    {path: "**", redirectTo: "404", pathMatch: 'full'},
 ];
 
