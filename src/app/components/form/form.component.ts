c2388795f0af0a179c8e6bca85e74e9c06434619
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-06-13 18:21:15 +0200

    spiegazione form

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit, ViewChild, viewChild } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-form',
   standalone: true,
   imports: [
     FormsModule,
+
     ReactiveFormsModule  
   ],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent implements OnInit {
-
   formSwitch =false 
 
   //OPTIMIZE TEMPLATE FORM
   @ViewChild("templateForm") templateForm!: NgForm
   templateSubmit(templateForm: NgForm){
-    console.log('templateForm', this.templateForm.value);
+    console.log('templateForm', this.templateForm);
   }
+
 
   // TODO REACTIVE FORM
   reactiveForm!: FormGroup;
@@ -28,13 +28,11 @@ export class FormComponent implements OnInit {
     this.reactiveForm =new FormGroup({
       username: new FormControl('', Validators.required),
       email: new FormControl('', 
-        [Validators.required, Validators.email]),
-
+        [ Validators.required, Validators.email ]),
     })
   }
-
+  
   reactiveSubmit(){
     console.log('reactiveForm', this.reactiveForm);
-    
   }
 }
