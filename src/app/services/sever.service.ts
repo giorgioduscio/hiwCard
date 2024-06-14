104082a53be651afbdc19296e7b63ae5f6a226b7
giorgio <giorgio.duscio@gmail.com>
2024-06-14 12:15:20 +0200

    servizio (non funsiona)

---
+++ src/app/services/sever.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+export interface Shop {
+  id: number;
+  denominazione: string;
+  indirizzo: string;
+  citta: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SeverService {
+  constructor(private http: HttpClient) {}
+
+  private searchSubject = new BehaviorSubject<string>('');
+  search$ = this.searchSubject.asObservable();
+
+  private url = 'https://server-node-igna.vercel.app/shops';
+
+  getShops(): Observable<Shop[]> {
+    return this.http.get<Shop[]>(this.url);
+  }
+  
+  getShopsByCity(city: string): Observable<Shop[]> {
+    return this.http.get<Shop[]>(this.url+"/"+city);
+  }
+
+  updateSearchTerm(term: string) {
+    this.searchSubject.next(term);
+  }
+  
+}
