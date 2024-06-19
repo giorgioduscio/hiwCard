1b822e9343ca55abd3ba7026c8871523df04e4ef
Giorgio_D'uscio <giorgio.duscio@gmail.com>
2024-06-19 12:32:03 +0200

    abilities check

--- src/app/components/card/score/score.component.ts
+++ src/app/components/card/score/score.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-score',
@@ -8,6 +8,27 @@ import { Component, Input } from '@angular/core';
   styleUrl: './score.component.css'
 })
 export class ScoreComponent {
- @Input() el:any
- showScoreComponent:boolean =true
+  @Input() el:any
+  showScoreComponent:boolean =true
+
+  // CHECK ABILITAà IN TEMPO REALE
+  setCheck(e:any, scoreIndex:number, abilityIndex:number){
+    this.el.characterScores[scoreIndex].abilities[abilityIndex].proficiency 
+    =e.target.checked
+  }
+
+  // COMPETENZA NELLE ABILITA'
+  abilityBonus(proficiency:boolean, scoreBonus:number){
+    let result
+    // SE HA COMPETENZA NOMINALE, AGGIUNGI LA COMPETENZA
+    if(proficiency) {
+      result =this.el.proficiencyBonus +scoreBonus
+    }else{
+      result =scoreBonus
+    }
+
+    if(result>-1) result ="+"+result
+
+    return result
+  }
 }
